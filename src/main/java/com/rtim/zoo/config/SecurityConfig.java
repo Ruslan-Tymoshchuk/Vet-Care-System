a797f1a6175963a20f15df6a27a43c2657f7d10f
R-Tim <R-Tim@DESKTOP-U139KSB>
2022-08-10 14:48:49 +0300

    added security and password encoder

---
+++ src/main/java/com/rtim/zoo/config/SecurityConfig.java
@@ -0,0 +1,27 @@
+package com.rtim.zoo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+                   .authorizeRequests()
+                   .antMatchers("/api/v1/auth/login").permitAll()
+                   .antMatchers("/api/v1/auth/registration").permitAll()
+                   .anyRequest().authenticated();
+        return http.build();
+    }
+    
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
