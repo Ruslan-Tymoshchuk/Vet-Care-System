98b7b08739e3307245f3fd5c41aad743a51d5182
R-Tim <R-Tim@host.docker.internal>
2024-02-02 01:36:58 +0100

    improved security config for using with JWT token

--- src/main/java/com/manager/animallist/config/SecurityConfig.java
+++ src/main/java/com/manager/animallist/config/SecurityConfig.java
@@ -3,21 +3,36 @@ package com.manager.animallist.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.core.GrantedAuthorityDefaults;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.access.intercept.FilterSecurityInterceptor;
+import com.manager.animallist.controller.filter.JwtAuthenticationFilter;
+import lombok.RequiredArgsConstructor;
 
 @Configuration
+@RequiredArgsConstructor
 public class SecurityConfig {
 
+    private final JwtAuthenticationFilter jwtAuthFilter;
+    
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.csrf().disable()
                    .authorizeRequests()
                    .antMatchers("/api/v1/auth/login").permitAll()
                    .antMatchers("/api/v1/auth/registration").permitAll()
-                   .anyRequest().authenticated();
+                   .anyRequest()
+                   .authenticated()
+                   .and()
+                   .addFilterBefore(jwtAuthFilter, FilterSecurityInterceptor.class);
         return http.build();
+    }
+    
+    @Bean
+    GrantedAuthorityDefaults grantedAuthorityDefaults() {
+        return new GrantedAuthorityDefaults("");
     }
     
     @Bean
