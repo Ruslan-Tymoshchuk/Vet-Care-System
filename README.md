2b33ca47ebd016cec19ad2757936883f1649691d
R-Tim <R-Tim@host.docker.internal>
2024-03-04 00:29:49 +0100

    some optimizations, updated readme

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
 # Animal-List-Management-Service
 
-Register and authenticate the user 
+Non-registered user has the opportunity to check, this email is available for registration 
 
-Authorized user gets full access to their animals
+After registration the user are authorized, in one request
 
 The user has 10 attempts to enter an incorrect password, or - block for 1 hour
+
+Authorized user gets full access to their animals
--- src/main/java/com/manager/animallist/exception/ExceptionHandlerAdvice.java
+++ src/main/java/com/manager/animallist/exception/ExceptionHandlerAdvice.java
@@ -3,6 +3,9 @@ package com.manager.animallist.exception;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.NoSuchElementException;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.LockedException;
@@ -17,16 +20,10 @@ public class ExceptionHandlerAdvice {
         return new ApiError(HttpStatus.NOT_FOUND, exception);
     }
 
-    @ExceptionHandler(ResourceNotFoundException.class)
+    @ExceptionHandler(NoSuchElementException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
-    public ApiError handleExceptionResourceNotFound(ResourceNotFoundException exception) {
+    public ApiError handleExceptionResourceNotFound(NoSuchElementException exception) {
         return new ApiError(HttpStatus.NOT_FOUND, exception);
-    }
-
-    @ExceptionHandler(UsernameAlredyExistsException.class)
-    @ResponseStatus(HttpStatus.CONFLICT)
-    public ApiError handleExceptionUsernameAlredyExists(UsernameAlredyExistsException exception) {
-        return new ApiError(HttpStatus.CONFLICT, exception);
     }
 
     @ExceptionHandler(BadCredentialsException.class)
