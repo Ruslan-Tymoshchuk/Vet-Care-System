21fa7ebfed6f6b7e13f7192c392766e5280d350e
R-Tim <R-Tim@host.docker.internal>
2024-03-26 21:56:38 +0100

    added jwt exceptions advices

--- src/main/java/com/manager/animallist/controller/filter/JwtAuthenticationFilter.java
+++ src/main/java/com/manager/animallist/controller/filter/JwtAuthenticationFilter.java
@@ -3,7 +3,7 @@ package com.manager.animallist.controller.filter;
 import static org.springframework.web.util.WebUtils.getCookie;
 import static org.springframework.util.StringUtils.startsWithIgnoreCase;
 import static com.manager.animallist.payload.JWTMarkers.*;
-import static  javax.servlet.http.HttpServletResponse.SC_UNAUTHORIZED;
+import static javax.servlet.http.HttpServletResponse.SC_UNAUTHORIZED;
 import java.io.IOException;
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
@@ -19,7 +19,9 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
+import org.springframework.web.servlet.HandlerExceptionResolver;
 import com.manager.animallist.service.JwtService;
+import io.jsonwebtoken.JwtException;
 
 @Component
 @RequiredArgsConstructor
@@ -27,33 +29,38 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
     private final JwtService jwtService;
     private final UserDetailsService userDetailsService;
+    private final HandlerExceptionResolver handlerExceptionResolver;
 
     @Override
     protected void doFilterInternal(@NonNull HttpServletRequest request, @NonNull HttpServletResponse response,
             @NonNull FilterChain filterChain) throws ServletException, IOException {
-        Cookie accessToken = getCookie(request, ACCESS_TOKEN);
-        Cookie refreshToken = getCookie(request, REFRESH_TOKEN);
-        if (accessToken != null && refreshToken != null
-                && startsWithIgnoreCase(accessToken.getValue(), BEARER_TOKEN_TYPE)
-                && startsWithIgnoreCase(refreshToken.getValue(), BEARER_TOKEN_TYPE)) {
-            final String jwtAccessToken = accessToken.getValue().substring(7);
-            final String jwtRefreshToken = refreshToken.getValue().substring(7);            
-            if (!jwtService.isTokenBlacklisted(jwtAccessToken) && !jwtService.isTokenBlacklisted(jwtRefreshToken)) {
-                final String userEmail = jwtService.extractUserEmail(jwtAccessToken);
-                if (!userEmail.isBlank() && SecurityContextHolder.getContext().getAuthentication() != null) {
-                    UserDetails userDetails = userDetailsService.loadUserByUsername(userEmail);
-                    if (jwtService.validate(jwtAccessToken, userDetails)) {
-                        UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(
-                                userDetails, null, userDetails.getAuthorities());
-                        authenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
-                        SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+        try {
+            Cookie accessToken = getCookie(request, ACCESS_TOKEN);
+            Cookie refreshToken = getCookie(request, REFRESH_TOKEN);
+            if (accessToken != null && refreshToken != null
+                    && startsWithIgnoreCase(accessToken.getValue(), BEARER_TOKEN_TYPE)
+                    && startsWithIgnoreCase(refreshToken.getValue(), BEARER_TOKEN_TYPE)) {
+                final String jwtAccessToken = accessToken.getValue().substring(7);
+                final String jwtRefreshToken = refreshToken.getValue().substring(7);
+                if (!jwtService.isTokenBlacklisted(jwtAccessToken) && !jwtService.isTokenBlacklisted(jwtRefreshToken)) {
+                    final String userEmail = jwtService.extractUserEmail(jwtAccessToken);
+                    if (!userEmail.isBlank() && SecurityContextHolder.getContext().getAuthentication() != null) {
+                        UserDetails userDetails = userDetailsService.loadUserByUsername(userEmail);
+                        if (jwtService.validate(jwtAccessToken, userDetails)) {
+                            UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(
+                                    userDetails, null, userDetails.getAuthorities());
+                            authenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                            SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+                        }
                     }
+                } else {
+                    response.setStatus(SC_UNAUTHORIZED);
+                    return;
                 }
-            } else {
-                response.setStatus(SC_UNAUTHORIZED);
-                return;
             }
+            filterChain.doFilter(request, response);
+        } catch (JwtException e) {
+            handlerExceptionResolver.resolveException(request, response, null, e);
         }
-        filterChain.doFilter(request, response);
     }
 }
--- src/main/java/com/manager/animallist/exception/ExceptionHandlerAdvice.java
+++ src/main/java/com/manager/animallist/exception/ExceptionHandlerAdvice.java
@@ -6,6 +6,8 @@ import org.springframework.web.HttpMediaTypeNotSupportedException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.security.SignatureException;
 import java.util.NoSuchElementException;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.LockedException;
@@ -43,5 +45,17 @@ public class ExceptionHandlerAdvice {
     public ExceptionHandlerResponse handleHttpMediaTypeNotSupportedException(
             HttpMediaTypeNotSupportedException exception) {
         return new ExceptionHandlerResponse(now(), NOT_ACCEPTABLE.value(), NOT_ACCEPTABLE, exception.getMessage());
+    }
+
+    @ExceptionHandler(ExpiredJwtException.class)
+    @ResponseStatus(UNAUTHORIZED)
+    public ExceptionHandlerResponse handleExpiredJwtException(ExpiredJwtException exception) {
+        return new ExceptionHandlerResponse(now(), UNAUTHORIZED.value(), UNAUTHORIZED, exception.getMessage());
+    }
+
+    @ExceptionHandler(SignatureException.class)
+    @ResponseStatus(UNAUTHORIZED)
+    public ExceptionHandlerResponse handleSignatureJWTException(SignatureException exception) {
+        return new ExceptionHandlerResponse(now(), UNAUTHORIZED.value(), UNAUTHORIZED, exception.getMessage());
     }
 }
