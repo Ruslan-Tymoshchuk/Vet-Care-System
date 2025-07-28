7eedb8437ccd00886c28a6e5af2b5fb78e849b9e
Ruslan Tymoshchuk <tim.ruslan14@gmail.com>
2025-07-28 14:34:58 +0200

    improved user sql's schemas

--- src/main/resources/db/scripts/1_build-table-users.sql
+++ src/main/resources/db/scripts/1_build-table-users.sql
@@ -4,6 +4,9 @@ CREATE TABLE users (
     last_name VARCHAR NOT NULL,
     email VARCHAR NOT NULL UNIQUE,
     password VARCHAR NOT NULL,
-    dt_login TIMESTAMP NOT NULL,
-    account_non_locked BOOLEAN 
+    account_non_expired BOOLEAN NOT NULL,
+    account_non_locked BOOLEAN NOT NULL,
+    credentials_non_expired BOOLEAN NOT NULL,
+    enabled BOOLEAN NOT NULL,
+    dt_login TIMESTAMP NOT NULL
 );
--- src/main/resources/db/scripts/4_insert-data.sql
+++ src/main/resources/db/scripts/4_insert-data.sql
@@ -1,17 +1,17 @@
-INSERT INTO users (first_name, last_name, email, password, dt_login, account_non_locked) 
-VALUES ('Emily', 'Smith', 'emily@gmail.com','$2a$12$d1XrgmOS0E30LrNWEjAOF.lKpkc/57LX7KqzXfH23UqzDK3DGdeBS', '2022-08-08 01:00:00', true), 
-       ('Dustin', 'Martinez', 'dustin@gmail.com', '$2a$12$L12LuqZWgvKxSnxWHEGdxOObFZevCxBg7tSUJ9VSpmk/KFl/dGnPq', '2022-08-08 02:00:00', true), 
-       ('Katie', 'Anderson', 'katie@gmail.com', '$2a$12$pU94kYmr60IoPMhdjvsab.4RG4oDjQLXalIdxyvBUoXxNiWins.8a', '2022-08-08 03:00:00', true), 
-       ('Emma', 'Taylor', 'emma@gmail.com', '$2a$10$fD.lmz9CqWzjA9XerPqtFOj/w3iucFuKMAYcIyz0tL1Nuj4tRCs72', '2022-08-08 04:00:00', true), 
-       ('Dominique', 'Moore', 'dominique@gmail.com', '$2a$10$yKQrDcmYs5y4.jpicX08BO0JUa/Ef9SmmDjUhDhckwXqLkR9WnIfS', '2022-08-08 05:00:00', true);
+INSERT INTO users (first_name, last_name, email, password, account_non_expired, account_non_locked, credentials_non_expired, enabled, dt_login) 
+VALUES ('Emily', 'Smith', 'emily@gmail.com','$2a$12$d1XrgmOS0E30LrNWEjAOF.lKpkc/57LX7KqzXfH23UqzDK3DGdeBS', true, true, true, true, '2022-08-08 01:00:00'), 
+       ('Dustin', 'Martinez', 'dustin@gmail.com', '$2a$12$L12LuqZWgvKxSnxWHEGdxOObFZevCxBg7tSUJ9VSpmk/KFl/dGnPq', true, true, true, true, '2022-08-08 02:00:00'), 
+       ('Katie', 'Anderson', 'katie@gmail.com', '$2a$12$pU94kYmr60IoPMhdjvsab.4RG4oDjQLXalIdxyvBUoXxNiWins.8a', true, true, true, true, '2022-08-08 03:00:00'), 
+       ('Emma', 'Taylor', 'emma@gmail.com', '$2a$10$fD.lmz9CqWzjA9XerPqtFOj/w3iucFuKMAYcIyz0tL1Nuj4tRCs72', true, true, true, true, '2022-08-08 04:00:00'), 
+       ('Dominique', 'Moore', 'dominique@gmail.com', '$2a$10$yKQrDcmYs5y4.jpicX08BO0JUa/Ef9SmmDjUhDhckwXqLkR9WnIfS', true, true, true, true, '2022-08-08 05:00:00');
        
 --password: test_password
 
-INSERT INTO roles (role_type)
+INSERT INTO authorities (title)
 VALUES ('USER'),
        ('ADMIN');
        
-INSERT INTO user_roles (user_id, role_id)
+INSERT INTO user_authorities (user_id, authority_id)
 VALUES (1, 1),
        (2, 1),
        (3, 1),
