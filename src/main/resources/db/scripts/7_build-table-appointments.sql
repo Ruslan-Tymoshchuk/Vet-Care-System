bf221e410f14297733c8594500cd1385ea9cfddc
Ruslan Tymoshchuk <ruslantymoshchuk14@gmail.com>
2025-07-31 11:15:52 +0200

    added appointments table

---
+++ src/main/resources/db/scripts/7_build-table-appointments.sql
@@ -0,0 +1,10 @@
+CREATE TABLE appointments (
+    id INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    veterinarian_id INTEGER NOT NULL REFERENCES veterinarians(id),
+    owner_id INTEGER NOT NULL REFERENCES owners(id),
+    pet_id INTEGER NOT NULL REFERENCES pets(id),
+    visit_date DATE,
+    visit_time TIME,
+    room VARCHAR NOT NULL,
+    type_of_visit VARCHAR NOT NULL  
+);
