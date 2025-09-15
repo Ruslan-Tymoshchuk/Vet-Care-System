d2bf99324f37852911929edc49360fcf0ab7216b
Ruslan-Tymoshchuk <ruslantymoshchuk14@gmail.com>
2025-09-15 22:54:18 +0200

    refactored access token filter

--- src/main/java/com/system/vetcare/security/filter/JwtAuthenticationFilter.java
+++ src/main/java/com/system/vetcare/security/filter/JwtAuthenticationFilter.java
@@ -11,12 +11,12 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.lang.NonNull;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 import org.springframework.web.servlet.HandlerExceptionResolver;
+import com.system.vetcare.domain.User;
 import com.system.vetcare.service.CookiesService;
 import com.system.vetcare.service.JwtService;
 import io.jsonwebtoken.JwtException;
@@ -24,8 +24,6 @@ import io.jsonwebtoken.JwtException;
 @Component
 @RequiredArgsConstructor
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
-
-    public static final String TOKENS_ARE_BLACKLISTED = "Tokens are blacklisted";
 
     private final JwtService jwtService;
     private final CookiesService cookiesService;
@@ -36,26 +34,28 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
     protected void doFilterInternal(@NonNull HttpServletRequest request, @NonNull HttpServletResponse response,
             @NonNull FilterChain filterChain) throws ServletException, IOException {
         try {
-            validateAuthenticationTokenRequest(request);
+            validateAccessTokenRequest(request);
             filterChain.doFilter(request, response);
         } catch (JwtException e) {
             handlerExceptionResolver.resolveException(request, response, null, e);
         }
     }
 
-    private void validateAuthenticationTokenRequest(HttpServletRequest request) {
-        Map<String, String> jwtTokens = cookiesService.extractJwtTokens(request.getCookies());
-        if(!jwtTokens.isEmpty()) {
-            final String jwtAccessToken = jwtTokens.get(ACCESS_TOKEN);
-            String email = jwtService.parseToken(jwtAccessToken).getSubject();
-            if (!email.isBlank() && SecurityContextHolder.getContext().getAuthentication() == null) {
-                UserDetails userDetails = userDetailsService.loadUserByUsername(email);
-                UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(
-                        userDetails, null, userDetails.getAuthorities());
-                authenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
-                SecurityContextHolder.getContext().setAuthentication(authenticationToken);
-            }
-        }
-    }
-
+	private void validateAccessTokenRequest(HttpServletRequest request) {
+		Map<String, String> jwtTokens = cookiesService.extractJwtTokens(request.getCookies());
+		if (jwtTokens.containsKey(ACCESS_TOKEN)) {
+			final String jwtAccessToken = jwtTokens.get(ACCESS_TOKEN);
+			if (!jwtService.isTokenBlacklisted(jwtAccessToken)) {
+				final String email = jwtService.parse(jwtAccessToken).getSubject();
+				if (!email.isBlank() && SecurityContextHolder.getContext().getAuthentication() == null) {
+					User user = (User) userDetailsService.loadUserByUsername(email);
+					UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(
+							user, null, user.getAuthorities());
+					authenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+					SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+				}
+			}
+		}
+	}
+	
 }
