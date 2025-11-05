45ba3742f1ab5d17c00c88669223d90994424ecc
Ruslan-Tymoshchuk <ruslantymoshchuk14@gmail.com>
2025-11-05 15:01:17 +0100

    refactored: improved leave date extraction logic

--- src/main/java/com/system/vetcare/service/LeaveService.java
+++ src/main/java/com/system/vetcare/service/LeaveService.java
@@ -1,14 +1,10 @@
 package com.system.vetcare.service;
 
 import java.time.LocalDate;
-import java.util.List;
 import java.util.Set;
-import com.system.vetcare.domain.Leave;
 
 public interface LeaveService {
 
-    List<Leave> findByStaffMemberAndDateInterval(Integer staffId, LocalDate start, LocalDate end);
-
-    Set<LocalDate> extractAbsentDays(List<Leave> leaves);
+    Set<LocalDate> extractAbsentDays(Integer staffId, LocalDate beginDate, LocalDate completeDate);
 
 }
--- src/main/java/com/system/vetcare/service/impl/LeaveServiceImpl.java
+++ src/main/java/com/system/vetcare/service/impl/LeaveServiceImpl.java
@@ -2,10 +2,8 @@ package com.system.vetcare.service.impl;
 
 import static java.util.stream.Collectors.toSet;
 import java.time.LocalDate;
-import java.util.List;
 import java.util.Set;
 import org.springframework.stereotype.Service;
-import com.system.vetcare.domain.Leave;
 import com.system.vetcare.repository.LeaveRepository;
 import com.system.vetcare.service.LeaveService;
 import lombok.RequiredArgsConstructor;
@@ -15,16 +13,12 @@ import lombok.RequiredArgsConstructor;
 public class LeaveServiceImpl implements LeaveService {
 
     private final LeaveRepository leaveRepository;
-    
-    @Override
-    public List<Leave> findByStaffMemberAndDateInterval(Integer staffId, LocalDate start, LocalDate end) {
-        return leaveRepository.findAllInDateInterval(staffId, start, end);
-    }
 
     @Override
-    public Set<LocalDate> extractAbsentDays(List<Leave> leaves) {
-        return leaves.stream().flatMap(leave -> leave.getBeginDate().datesUntil(leave.getCompleteDate().plusDays(1)))
+    public Set<LocalDate> extractAbsentDays(Integer staffId, LocalDate beginDate, LocalDate completeDate) {
+        return leaveRepository.findAllInDateInterval(staffId, beginDate, completeDate).stream()
+                .flatMap(leave -> leave.getBeginDate().datesUntil(leave.getCompleteDate().plusDays(1)))
                 .collect(toSet());
     }
-    
+
 }
