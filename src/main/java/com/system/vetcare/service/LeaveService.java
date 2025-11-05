2560ccba00450156cf99b302c5c61974819f298a
Ruslan-Tymoshchuk <ruslantymoshchuk14@gmail.com>
2025-11-05 14:42:56 +0100

    added additional behavior for leaves

--- src/main/java/com/system/vetcare/service/LeaveService.java
+++ src/main/java/com/system/vetcare/service/LeaveService.java
@@ -2,10 +2,13 @@ package com.system.vetcare.service;
 
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Set;
 import com.system.vetcare.domain.Leave;
 
-public interface LeavesService {
+public interface LeaveService {
 
     List<Leave> findByStaffMemberAndDateInterval(Integer staffId, LocalDate start, LocalDate end);
+
+    Set<LocalDate> extractAbsentDays(List<Leave> leaves);
 
 }
