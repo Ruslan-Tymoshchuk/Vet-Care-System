f2d9867a927b14926eb49f52cb5f73bdc6d3ec50
Ruslan-Tymoshchuk <ruslantymoshchuk14@gmail.com>
2025-11-10 18:38:37 +0100

    some adjustments

--- src/main/java/com/system/vetcare/payload/response/AvailableDateResponse.java
+++ src/main/java/com/system/vetcare/payload/response/AvailableDateResponse.java
@@ -1,9 +1,16 @@
 package com.system.vetcare.payload.response;
 
+import static java.time.format.DateTimeFormatter.ISO_LOCAL_DATE;
+import java.time.LocalDate;
 import java.util.List;
 
 public record AvailableDateResponse(
         String date, 
-        Boolean isAvailable,
+        Boolean isAvailable, 
         List<AppointmentTimeSlotResponse> timeSlots) {
+    
+    public AvailableDateResponse(LocalDate date, Boolean isAvailable, List<AppointmentTimeSlotResponse> timeSlots) {
+        this(date.format(ISO_LOCAL_DATE), isAvailable, timeSlots);
+    }
+    
 }
