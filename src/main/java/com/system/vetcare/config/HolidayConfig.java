b99dfca6cc6fcd46bfb34a81c6ac35d37529b8bc
Ruslan-Tymoshchuk <ruslantymoshchuk14@gmail.com>
2025-11-12 03:17:58 +0100

    implemented holiday days validation in vet availability calculation

---
+++ src/main/java/com/system/vetcare/config/HolidayConfig.java
@@ -0,0 +1,21 @@
+package com.system.vetcare.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import de.jollyday.HolidayCalendar;
+import de.jollyday.HolidayManager;
+import de.jollyday.ManagerParameters;
+
+@Configuration
+public class HolidayConfig {
+
+    @Value("${holiday.calendar}")
+    private String holidayCalendar;
+
+    @Bean
+    public HolidayManager holidayManager() {
+        return HolidayManager.getInstance(ManagerParameters.create(HolidayCalendar.valueOf(holidayCalendar)));
+    }
+
+}
