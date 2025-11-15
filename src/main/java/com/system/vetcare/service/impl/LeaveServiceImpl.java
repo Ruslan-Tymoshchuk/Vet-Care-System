f5b3adb6463323a17c7f7700c603ec5b37a5e49b
Ruslan-Tymoshchuk <ruslantymoshchuk14@gmail.com>
2025-11-15 20:34:02 +0100

    some improvements

--- src/main/java/com/system/vetcare/service/impl/LeaveServiceImpl.java
+++ src/main/java/com/system/vetcare/service/impl/LeaveServiceImpl.java
@@ -2,8 +2,10 @@ package com.system.vetcare.service.impl;
 
 import static java.util.stream.Collectors.toSet;
 import java.time.LocalDate;
+import java.util.List;
 import java.util.Set;
 import org.springframework.stereotype.Service;
+import com.system.vetcare.domain.Leave;
 import com.system.vetcare.repository.LeaveRepository;
 import com.system.vetcare.service.LeaveService;
 import lombok.RequiredArgsConstructor;
@@ -15,10 +17,13 @@ public class LeaveServiceImpl implements LeaveService {
     private final LeaveRepository leaveRepository;
 
     @Override
-    public Set<LocalDate> extractAbsentDays(Integer staffId, LocalDate beginDate, LocalDate completeDate) {
-        return leaveRepository.findAllInDateInterval(staffId, beginDate, completeDate).stream()
-                .flatMap(leave -> leave.getBeginDate().datesUntil(leave.getCompleteDate().plusDays(1)))
-                .collect(toSet());
+    public Set<LocalDate> extractVeterinarianLeaveDays(Integer veterinarianId, LocalDate beginDate, LocalDate completeDate) {
+        return extractDays(leaveRepository.findByVeterinarianInDateInterval(veterinarianId, beginDate, completeDate));
     }
 
+    private Set<LocalDate> extractDays(List<Leave> leaves) {
+        return leaves.stream().flatMap(leave -> leave.getBeginDate().datesUntil(leave.getCompleteDate().plusDays(1)))
+                .collect(toSet());
+    }
+    
 }
